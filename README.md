c9c8b320feb08b86bd1e379671c53d9716e9eae9
K. Adam White <kadamwhite@users.noreply.github.com>
2019-03-06 13:44:37 -0500

    Copy relevant logic from tester extension

---
+++ README.md
@@ -0,0 +1,41 @@
+# PHPUnit extension for Chassis
+
+Install PHPUnit within your [Chassis](http://chassis.io/) box! The PHPUnit extension automatically sets up your Chassis instance with the `phpunit` executable, for use running PHP unit tests.
+
+## Activation
+
+### Automated Installation
+
+To add the phpunit extension to a Chassis box, include `chassis/phpunit` in the `extensions` list within your `config.local.yaml` file:
+
+```yml
+extensions:
+    - chassis/phpunit
+```
+
+Then run `vagrant provision` to instruct Chassis to download and install the new extension.
+
+### Manual Installation
+
+Ensure you have a Chassis instance set up locally already.
+
+```
+# In your Chassis dir:
+cd extensions
+
+# Grab the extension
+git clone --recursive https://github.com/Chassis/phpunit.git phpunit
+
+# Reprovision
+cd ..
+vagrant provision
+```
+
+# Usage
+```
+vagrant ssh
+cd /vagrant/content/{plugins|themes}/yourdirectory
+phpunit
+```
+
+You're now ready to run `phpunit` within your Chassis virtual machine!
---
+++ modules/tester/manifests/init.pp
@@ -0,0 +1,56 @@
+# PHPUnit extension for Chassis
+class phpunit (
+	$config,
+	$install_path = '/usr/local/src/phpunit',
+) {
+
+	if ( !empty($config[disabled_extensions]) and 'chassis/phpunit' in $config[
+		disabled_extensions] ) {
+		$phpunit = absent
+	} else {
+		$phpunit = present
+	}
+
+	if ( present == $phpunit ) {
+		# Create the install path
+		file { $install_path:
+			ensure => directory,
+		}
+
+		if versioncmp( $config[php], '5.6' ) == 0 {
+			$phpunit_repo_url = 'https://phar.phpunit.de/phpunit-4.8.phar'
+		} else {
+			$phpunit_repo_url = 'https://phar.phpunit.de/phpunit-5.7.phar'
+		}
+
+		# Download phpunit
+		exec { 'phpunit download':
+			command => "/usr/bin/curl -o ${install_path}/phpunit.phar -L ${
+				phpunit_repo_url}",
+			require => [ Package[ 'curl' ], File[ $install_path ] ],
+			creates => "${install_path}/phpunit.phar",
+		}
+
+		# Ensure we can run phpunit
+		file { "${install_path}/phpunit.phar":
+			ensure  => present,
+			mode    => 'a+x',
+			require => Exec[ 'phpunit download' ]
+		}
+
+		# Symlink it across
+		file { '/usr/bin/phpunit':
+			ensure  => link,
+			target  => "${install_path}/phpunit.phar",
+			require => File[ "${install_path}/phpunit.phar" ],
+		}
+	} else {
+		file { $install_path:
+			ensure => $phpunit,
+			force  => true
+		}
+		file { '/usr/bin/phpunit':
+			ensure => absent
+		}
+	}
+}
